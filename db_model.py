df92dd016d6eb342d6e0ef3f08becc4182a5d8bc
olmits <42008856+olmits@users.noreply.github.com>
2018-12-30 20:41:28 +0200

    Update db_model.py

--- db_model.py
+++ db_model.py
@@ -1,6 +1,3 @@
-# TODO отрефакторить под rates_parser
-
-
 from peewee import Model, SqliteDatabase, CharField, DoubleField, DateTimeField
 from rates_parser import Parsing
 from datetime import datetime
@@ -20,31 +17,22 @@ class XRate(Model):
     updated = DateTimeField()
 
     def __str__(self):
-        return f"{self.updated.strftime('%Y-%m-%d')} - {self.source}: {self.cc} {self.ask}"
+        return f"{self.updated} - {self.source}: {self.cc} {self.ask}"
 
 
 # XRate.drop_table()
 XRate.create_table()
 
 
-class Updating:
+def update_rates(api_source, update_date=datetime.now()):
 
-    def processing(self):
-
-
-
-
-
-def update_rates(source, update_date=datetime.now()):
-
-    source = Parsing(source, update_date)
-    source.
-
-    act_rate_info = func(update_date)  # Send API request and get records
-    for cc, ask, bid, updated in act_rate_info:
+    parsed = Parsing(api_source, update_date)
+    parsed.processing()
+    # act_rate_info = func(update_date)
+    for cc, ask, bid, updated in parsed.data:
         updated = updated.replace(hour=0, minute=0, second=0, microsecond=0)
         existing = XRate.select().where(  # Check if there're already this query
-            XRate.source == func.__name__,  # by a source
+            XRate.source == api_source,  # by a source
             XRate.cc == cc,  # by a currency
             XRate.updated == updated  # and by a date
         )
@@ -55,26 +43,13 @@ def update_rates(source, update_date=datetime.now()):
             existing.save()
         else:
             XRate.create(  # If not, create new query
-                source=func.__name__,
+                source=api_source,
                 cc=cc,
                 ask=ask,
                 bid=bid,
                 updated=updated
             )
 
-
-if __name__ == '__main__':
-    dat = datetime(2018, 12, 26)
-
-    res = Parsing('nbu', dat)
-    res.nbu_processing()
-    update_rates(res.data, dat)
-
-    # update_rates(mb, dat)
-    r0 = XRate.select()
-    r = r0.where(XRate.cc == 'USD').order_by(XRate.updated.asc())
-    for each in r:
-        print(each)
 
 db.close()
 
