c56dbe73e681e0765119a629095214e137e7ff67
olmits <42008856+olmits@users.noreply.github.com>
2019-01-03 21:03:57 +0200

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -1,16 +1,36 @@
 from collections import namedtuple
-from db_model import XRate
+from datetime import datetime
+from rates_parser import Parsing
+from db_model import XRate, update_rates
 
 
-def get_r():
-    last_updated = XRate.select().order_by(XRate.updated.desc()).first().updated  # Get the last updated date.
-    records = XRate.select().where(XRate.updated == last_updated)  # Get actual rates records.
+def update_bd():
+    actual_date = datetime.now()
+    for each in Parsing.api.keys():
+        update_rates(each, actual_date)
+
+
+def get_last_updated_cc(source, cc):
+    last_updated_date = XRate.select().where(XRate.source == source).order_by(XRate.updated.desc()).first().updated
+    last_updated_cc = XRate.select().where(
+        XRate.source == source,
+        XRate.updated == last_updated_date,
+        XRate.cc == cc
+    )
+    return last_updated_cc
+
+
+def get_all_last_updated_cc(cc='USD'):
+    records = []
+    for an_api in Parsing.api.keys():
+        for each in get_last_updated_cc(an_api, cc):
+            records.append(each)
     return records
 
 
 def filter_r(args):
-    cc = args.getlist("cc")
-    records = get_r().where(XRate.cc == cc)
+    cc = args.get("cc")
+    records = get_all_last_updated_cc(cc)
     return records
 
 
@@ -21,10 +41,8 @@ def create_tup(func):  # Create the list of namedtuple from the given function.
         query(
             rate.source,
             rate.cc,
-            round(float(rate.ask), 3),
-            round(float(rate.bid), 3))
+            round(float(rate.ask), 2),
+            round(float(rate.bid), 2))
         for rate in func
     ]
     return tubs
-
-
