087cdd1534ffbc8842295e4051234cf03f4a08ae
olmits <42008856+olmits@users.noreply.github.com>
2019-01-03 21:04:25 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,11 +1,11 @@
 from flask import redirect, url_for, render_template, request
 from rates_app import app
-# from db_model import update_rates, XRate
 import funcs
 
 
 @app.route("/")
 def home():
+    funcs.update_bd()
     return redirect(url_for("get_rates"))
 
 
@@ -14,6 +14,6 @@ def get_rates():
     if request.args:  # Requested rate
         get_records = funcs.filter_r(request.args)  # Requested, barely readable records.
     else:
-        get_records = funcs.get_r()  # All available, barely readable records.
+        get_records = funcs.get_all_last_updated_cc()  # All available, barely readable records.
     customize_it = funcs.create_tup(get_records)  # Readable and compact form to proceed for the next step.
-    return render_template("template1.html", rates=customize_it)
+    return render_template("main.html", rates=customize_it)
