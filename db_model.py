cf44685d22d185f0340027e208b66653aced52e1
olmits <42008856+olmits@users.noreply.github.com>
2019-01-21 17:22:51 +0200

    Update db_model.py

--- db_model.py
+++ db_model.py
@@ -1,6 +1,7 @@
 from peewee import Model, SqliteDatabase, CharField, DoubleField, DateTimeField
 from rates_parser import Parsing
-from datetime import datetime
+from minf_grabber import minfin_blck_mrck
+from datetime import datetime, date
 
 
 db = SqliteDatabase("rates.db")
@@ -17,19 +18,28 @@ class XRate(Model):
     updated = DateTimeField()
 
     def __str__(self):
-        return f"{self.updated} - {self.source}: {self.cc} {self.ask}"
+        return f"{self.updated} - {self.source}: {self.cc} Ask (продажа): {self.ask} Bid (покупка): {self.bid}"
 
 
 # XRate.drop_table()
 XRate.create_table()
 
 
+def get_rates():  # TODO add grabber rates processing
+    pass 
+
+
 def update_rates(api_source, update_date=datetime.now()):
 
-    parsed = Parsing(api_source, update_date)
-    parsed.processing()
-    # act_rate_info = func(update_date)
-    for cc, ask, bid, updated in parsed.data:
+    def get_rates(api_source):
+        if api_source in Parsing.api.keys():
+            parsed = Parsing(api_source, update_date)
+            parsed.processing()
+            return parsed.data
+        elif api_source == minfin_blck_mrck.__name__:
+            return minfin_blck_mrck()
+
+    for cc, ask, bid, updated in get_rates(api_source):
         updated = updated.replace(hour=0, minute=0, second=0, microsecond=0)
         existing = XRate.select().where(  # Check if there're already this query
             XRate.source == api_source,  # by a source
@@ -53,12 +63,16 @@ def update_rates(api_source, update_date=datetime.now()):
 
 if __name__ == '__main__':
 
-    # dat = datetime.now()
+    dat = datetime(2019, 1, 13)
     # for each in Parsing.api.keys():
     #     update_rates(each, dat)
 
-    for each in XRate.select().order_by(XRate.updated.desc()):
+    for each in XRate.select().where(XRate.cc == 'USD', XRate.updated == dat).order_by(XRate.updated.desc()):
         print(each)
+
+    distinct_list = XRate.select(XRate.source).distinct()
+    for each in distinct_list:
+        print(each.source)
 
 
 db.close()
